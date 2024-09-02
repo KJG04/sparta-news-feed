879fe4a3f1e5f6b273b477e7c5f5e7564259c6aa
KJG04 <freedom7113@gmail.com>
2024-09-02 10:50:13 +0900

    initial commit

---
+++ src/main/java/com/sparta/spartanewsfeed/SpartaNewsFeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartanewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaNewsFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaNewsFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartanewsfeed/SpartaNewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartanewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaNewsFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
