42e7829924cecc524dcf28d2622b7cbe7ed01a78
taeyoung kang <kty1467@naver.com>
2024-09-02 14:48:18 +0900

    add : User엔티티 추가, BaseEntity 추가

---
+++ src/main/java/com/sparta/spartanewsfeed/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.sparta.spartanewsfeed.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = { AuditingEntityListener.class})
+@Getter
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+}
