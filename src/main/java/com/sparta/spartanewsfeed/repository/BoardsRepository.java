2e21e8945321a5f10bd7ec37e29f3de885db6936
SuHyun <46856071+SuHyun-git@users.noreply.github.com>
2024-09-04 11:57:13 +0900

    친구와 자신의 글만 조회할 수 있도록 변경

--- src/main/java/com/sparta/spartanewsfeed/repository/BoardsRepository.java
+++ src/main/java/com/sparta/spartanewsfeed/repository/BoardsRepository.java
@@ -4,6 +4,9 @@ import com.sparta.spartanewsfeed.entity.Boards;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardsRepository extends JpaRepository<Boards, Long> {
+    List<Boards> findAllByUserIdIn(List<Long> userList);
 }
