eba619a9303ead6aed8ca2768834e6039df23121
KJG04 <freedom7113@gmail.com>
2024-09-04 13:44:38 +0900

    feat: 댓글 조회 기능 추가

--- src/main/java/com/sparta/spartanewsfeed/controller/CommentController.java
+++ src/main/java/com/sparta/spartanewsfeed/controller/CommentController.java
@@ -1,16 +1,40 @@
 package com.sparta.spartanewsfeed.controller;
 
+import com.sparta.spartanewsfeed.dto.CommentResponseDto;
+import com.sparta.spartanewsfeed.dto.UserResponseDto;
+import com.sparta.spartanewsfeed.entity.Comment;
+import com.sparta.spartanewsfeed.service.CommentService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/comments")
 public class CommentController {
+    final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
     @GetMapping()
-    void getAllComments() {
+    ResponseEntity<List<CommentResponseDto>> getAllComments() {
+        List<Comment> comments = commentService.getAllComments();
+        List<CommentResponseDto> commentResponseDtos = comments.stream()
+                .map(v ->
+                        CommentResponseDto.builder()
+                                .id(v.getId())
+                                .contents(v.getContents())
+                                .userResponseDto(new UserResponseDto(v.getUser()))
+                                .build()
+                ).toList();
+        return ResponseEntity.ok(commentResponseDtos);
     }
 
     @PostMapping()
     void addComment() {
+
     }
 
     @PatchMapping("/{commentId}")
--- src/main/java/com/sparta/spartanewsfeed/service/CommentService.java
+++ src/main/java/com/sparta/spartanewsfeed/service/CommentService.java
@@ -1,8 +1,21 @@
 package com.sparta.spartanewsfeed.service;
 
+import com.sparta.spartanewsfeed.entity.Comment;
+import com.sparta.spartanewsfeed.repository.CommentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentService {
-    
+    final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getAllComments() {
+        // 삭제되지 않은 유저의 댓글만
+        return commentRepository.getAllByUser_DeleteStatus(false);
+    }
 }
