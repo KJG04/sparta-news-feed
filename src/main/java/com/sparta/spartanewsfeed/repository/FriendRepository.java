5243c3515ee09350d1289e8fd505d98b9eddf7ee
taeyoung kang <kty1467@naver.com>
2024-09-04 11:50:06 +0900

    fix : 탈퇴한 유저 로그인 방지, 탈퇴 시 관련 데이터 삭제

--- src/main/java/com/sparta/spartanewsfeed/repository/FriendRepository.java
+++ src/main/java/com/sparta/spartanewsfeed/repository/FriendRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.spartanewsfeed.repository;
 
 import com.sparta.spartanewsfeed.entity.Friend;
+import com.sparta.spartanewsfeed.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,10 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
     Optional<Friend> findByFromUser_UserIdAndToUser_UserId(Long fromUser_userId, Long toUser_userId);
 
     List<Friend> findAllByFromUser_UserIdAndToUser_DeleteStatus(Long fromUser_userId, Boolean toUser_deleteStatus);
+
+    // 특정 유저가 요청한 모든 친구 관계 찾기
+    List<Friend> findAllByFromUser(User fromUser);
+
+    // 특정 유저가 수락한 모든 친구 관계 찾기
+    List<Friend> findAllByToUser(User toUser);
 }
