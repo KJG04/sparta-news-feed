d67302f3d95f4bc5b75bf9b1d0b041a9c3b02d69
SuHyun <46856071+SuHyun-git@users.noreply.github.com>
2024-09-04 20:58:16 +0900

    pagination 구현과 단건 조회 권한 부여, 단건 조회시 좋아요 개수와, 댓글 개수 추가

--- src/main/java/com/sparta/spartanewsfeed/service/BoardsLikeService.java
+++ src/main/java/com/sparta/spartanewsfeed/service/BoardsLikeService.java
@@ -1,6 +1,7 @@
 package com.sparta.spartanewsfeed.service;
 
 import com.sparta.spartanewsfeed.dto.BoardsLikeResponseDto;
+import com.sparta.spartanewsfeed.entity.Boards;
 import com.sparta.spartanewsfeed.entity.BoardsLike;
 import com.sparta.spartanewsfeed.entity.User;
 import com.sparta.spartanewsfeed.repository.BoardsLikeRepository;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class BoardsLikeService {
@@ -20,21 +23,21 @@ public class BoardsLikeService {
 
     @Transactional
     public BoardsLikeResponseDto boardsLike(Long boardId, User user) {
+
         BoardsLike boardsLike = boardsLikeRepository.findByBoardIdAndUserId(boardId, user.getUserId());
+        Boards board = boardsRepository.findById(boardId).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 게시판의 ID를 찾을 수 없습니다."));
+
         // 처음 좋아요를 누르는 경우 생성을 하고, 만약 전에 누른적이 있다면 상태를 변경합니다.
-        if(user.getUserId().equals(boardsLike.getUserId())){
+        if(user.getUserId().equals(board.getUserId())){
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "자신의 게시글에는 좋아요를 누를 수 없습니다.");
         }
+
         if (boardsLike == null) {
             BoardsLike newboardsLike = new BoardsLike(user.getUserId(), boardId, true);
             BoardsLike saveBoardsLike = boardsLikeRepository.save(newboardsLike);
             return new BoardsLikeResponseDto(saveBoardsLike);
         } else {
-            if(boardsLike.getLikeState()) {
-                return new BoardsLikeResponseDto(boardsLike.update());
-            } else{
-                return new BoardsLikeResponseDto(boardsLike.update());
-            }
+            return new BoardsLikeResponseDto(boardsLike.update());
         }
     }
 }
